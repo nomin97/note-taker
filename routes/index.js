558dde9ac9f142cee6297bd29b27bd9b7a009fff
Nomin Myagmarsuren <nominmyagmarsuren@gmail.com>
2023-11-29 00:08:48 -0700

    fixed error in heroku app hopefully

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const router = require ("express").Router();
 const path = require ("path");
-const apiroutes = require ("./API");
+const apiroutes = require ("./api");
 
 router.use ("/api", apiroutes);
 
